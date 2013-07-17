69cb73d054c2580bef9a425ed01e85529e16be3f
Ashton McCrate <a.mccrate@gmail.com>
2013-07-17 12:45:38 -0700

    Fix flash message style.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def flash_class(type)
+		case type
+		when :alert
+			return "alert-error"
+		when :notice
+			return "alert-info"
+		else
+			return '"'	
+		end
+	end
 end
